b4a577ff14ace02d179a89338d1d83ee3b981de0
xiaobiG <18376176154@163.com>
2026-08-30 10:48:05 +0800

    feat: surface lesson metadata in docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import 'katex/dist/katex.min.css'
 import './style.css'
 import BfsTraceExplorer from './components/BfsTraceExplorer.vue'
+import LessonMeta from './components/LessonMeta.vue'
 
 export default {
   extends: DefaultTheme,
+  Layout: () => h(DefaultTheme.Layout, null, {
+    'doc-before': () => h(LessonMeta),
+  }),
   enhanceApp({ app }) {
     app.component('BfsTraceExplorer', BfsTraceExplorer)
   },
